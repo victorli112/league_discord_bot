80ac562dba10afebf37e45c81eee42ac76f0f012
Victor Li <victor.li.7802@gmail.com>
2022-05-22 22:03:12 -0400

    comments

--- get_build.py
+++ get_build.py
@@ -5,8 +5,8 @@ import requests
 INPUT: champion_name: the champion to be queried
         role: the role of the champion
 OUTPUT: a dictionary consisting of all the items that is recommended.
-            NOTE: item_0 are the boots, item_6 is the ward, item_7 is the elixir
-DESCRIPTION:
+            NOTE: item_0 are the boots, item_6 is the ward, item_7 is the elixir. The other items are listed chronologically.
+DESCRIPTION: get the recommended build of the champion in the role
 """
 def get_build(champion_name: str, role: str) -> dict:
     url = f"https://www.metasrc.com/5v5/champion/{champion_name}/{role}"
@@ -21,7 +21,7 @@ def get_build(champion_name: str, role: str) -> dict:
         item = img['alt']
         items[f'item_{counter}'] = item
         counter += 1
-        
+
     return items
 
 get_build("azir", "mid")
