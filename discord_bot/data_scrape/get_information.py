405b4d743dc2b90a7d26acf81f3f0f91e9003fad
Victor Li <victor.li.7802@gmail.com>
2022-06-30 18:20:30 -0400

    struct

--- discord_bot/data_scrape/get_information.py
+++ discord_bot/data_scrape/get_information.py
@@ -5,6 +5,14 @@ from .get_starting_items import get_starting_items
 from .get_skills import get_skills
 from .get_summoner_spells import get_summoner_spells
 
+class ResponseStruct():
+    def __init__(self, runes, summoners, skills, starting_items, build):
+        self.runes = runes
+        self.summoners = summoners
+        self.skills = skills
+        self.starting_items = starting_items
+        self.build = build
+
 def get_information(champion_name: str, role: str=""):
     blocks = get_blocks(champion_name, role)
     runes = get_runes(blocks[0])
@@ -12,4 +20,5 @@ def get_information(champion_name: str, role: str=""):
     skills = get_skills(blocks[2])
     starting_items = get_starting_items(blocks[3])
     build = get_build(blocks[4])
-    return(skills)
+    response = ResponseStruct(runes, summoners, skills, starting_items, build)
+    return(response)
