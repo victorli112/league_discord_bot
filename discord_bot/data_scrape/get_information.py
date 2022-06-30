ead03cf79cab77012622b6f495811bce1fbdb7ea
Victor Li <victor.li.7802@gmail.com>
2022-06-30 17:44:04 -0400

    fix imports

--- discord_bot/data_scrape/get_information.py
+++ discord_bot/data_scrape/get_information.py
@@ -1,9 +1,9 @@
-from get_build import get_build
-from get_soup_blocks import get_blocks
-from get_runes import get_runes
-from get_starting_items import get_starting_items
-from get_skills import get_skills
-from get_summoner_spells import get_summoner_spells
+from .get_build import get_build
+from .get_soup_blocks import get_blocks
+from .get_runes import get_runes
+from .get_starting_items import get_starting_items
+from .get_skills import get_skills
+from .get_summoner_spells import get_summoner_spells
 
 def get_information(champion_name: str, role: str=""):
     blocks = get_blocks(champion_name, role)
