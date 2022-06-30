9ebf63a60562de0a96e3d3e1469d13a79f32c3ed
Victor Li <victor.li.7802@gmail.com>
2022-06-30 17:54:55 -0400

    input handling

--- discord_bot/discord_bot.py
+++ discord_bot/discord_bot.py
@@ -5,7 +5,7 @@ import random
 from data_scrape.get_information import get_information
 import os
 
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = 'OTI2OTAyOTA3MzEwMzIxNzU2.GWKn6K.yHOsE0Ga2wk6xsgM5q0l9Pm5IRufMXdIR7FcTc'
 
 POKEMON = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"]
 client = discord.Client()
@@ -18,15 +18,17 @@ async def on_message(message):
     command = message.content 
     # initial command
     if command.startswith('8=D'):
-        champion_and_role = command.split('8=D')[1]
-        champion_and_role = champion_and_role.split(" ")
-        if champion_and_role.size() == 1:
-            await message.channel.send("u dum dum do champ name and role")
-            return
-        build = get_information(champion_and_role[0], champion_and_role[1])
-        await message.channel.send(random.choice(POKEMON))
-        await message.channel.send(build)
-        
+        try: 
+            champion_and_role = command.split('8=D')[1].strip()
+            champion_and_role = champion_and_role.split(" ")
+            if len(champion_and_role) == 1:
+                build = get_information(champion_and_role[0])
+            else: 
+                 build = get_information(champion_and_role[0], champion_and_role[1])
+            await message.channel.send(random.choice(POKEMON))
+            await message.channel.send(build)
+        except Exception as err:
+            await message.channel.send(f"u dum dum do champ name and role, {err}")
 
 if __name__ == "__main__":
     client.run(TOKEN)
