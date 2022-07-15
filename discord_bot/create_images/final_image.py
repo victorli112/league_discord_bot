c3fed5a4b51dcdc078a494f8aab886c8109a9631
Victor Li <victor.li.7802@gmail.com>
2022-07-14 23:20:44 -0400

    remove pycache files

--- discord_bot/create_images/final_image.py
+++ discord_bot/create_images/final_image.py
@@ -6,7 +6,7 @@ OUTPUT: Image of all files combined
 DESCRIPTION: Create the final image with runes, build, starting items, and summoners
 """
 def build_final_image(image_list: list) -> Image:
-    final_image = Image.open('bot template.png')
+    final_image = Image.open('bot_template.png')
     coord_list = [(2,46), (354,46), (2,238), (226,238), (2,366)]
     for image, coords in zip(image_list, coord_list):
         img = Image.open(image)
